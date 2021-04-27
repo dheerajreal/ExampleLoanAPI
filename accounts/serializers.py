db08c7c697299c76eebfc8c329745257ead50cdf
Dheeraj <44266862+dheerajreal@users.noreply.github.com>
2021-04-27 21:19:12 +0530

    add serializers

---
+++ accounts/serializers.py
@@ -0,0 +1,46 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True,)
+
+    class Meta:
+        model = User
+        fields = [
+            'id',
+            'username',
+            'first_name',
+            'last_name',
+            'email',
+            'is_admin',
+            'is_agent',
+            'password',
+        ]
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'],
+            is_agent=validated_data['is_agent'],
+            is_admin=validated_data['is_admin'],
+            first_name=validated_data.get("first_name"),
+            last_name=validated_data.get("last_name"),
+            email=validated_data.get("email"),
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+
+class UserUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            'id',
+            'first_name',
+            'last_name',
+            'username',
+            'email',
+        ]
