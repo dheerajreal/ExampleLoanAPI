a76e207dac4f31bdc8f33b1631c1065b2f63e8b7
Dheeraj <44266862+dheerajreal@users.noreply.github.com>
2021-05-09 16:17:27 +0530

    improved permissions

--- accounts/permissions.py
+++ accounts/permissions.py
@@ -5,6 +5,7 @@ class AdminPermission(permissions.BasePermission):
     message = "User is not an admin"
 
     def has_permission(self, request, view):
+        # is_staff and is_superuser is for superuser and site maintainers
         return request.user.is_admin or request.user.is_staff
 
 
@@ -13,3 +14,10 @@ class AgentPermission(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return request.user.is_admin or request.user.is_agent
+
+
+class ExclusivelyAgentPermission(permissions.BasePermission):
+    message = "User is not an agent"
+
+    def has_permission(self, request, view):
+        return request.user.is_agent
