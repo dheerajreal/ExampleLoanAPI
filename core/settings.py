45003b0b7ea6e13fefbcc2408ae167cfc5f53607
Dheeraj <44266862+dheerajreal@users.noreply.github.com>
2021-05-09 22:27:29 +0530

    env variables and optional postgres backend

--- core/settings.py
+++ core/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+from decouple import Csv, config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,13 +22,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^spq)-^^b7q@=4ntmnm73y=k%h_5mms&(r9tl#d6306#6zeu^&'
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
+DEBUG = config('DEBUG', default=False, cast=bool)
+SECRET_KEY = config('SECRET_KEY', default="whatever_secret_key_for_prod")
+ALLOWED_HOSTS = config(
+    'ALLOWED_HOSTS',
+    default="127.0.0.1 ,localhost ,0.0.0.0",
+    cast=Csv()
+)
 
 # Application definition
 
@@ -85,6 +88,18 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+DB_ENGINE = config('DB_ENGINE', default="sqlite")
+if DB_ENGINE == "psql":
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': config('DB_NAME'),
+            'USER': config('DB_USER'),
+            'PASSWORD': config('DB_PASSWORD'),
+            'HOST': config('DB_HOST', default='127.0.0.1'),
+            'PORT': config('DB_PORT', default='5432'),
+        }
+    }
 
 
 # Password validation
