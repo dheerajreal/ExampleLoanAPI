a542b785bc1b88f1ccd2fbcba7211b4577576172
Dheeraj <44266862+dheerajreal@users.noreply.github.com>
2021-05-09 16:17:42 +0530

    added some more tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,10 +1,16 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.urls import reverse
 
 
 class UsersManagersTests(TestCase):
     def setUp(self):
         self.usermodel = get_user_model()
+        self.demouser = self.usermodel.objects.create_user(
+            username="demouser",
+            password='password',
+            is_admin=True
+        )
 
     def test_create_user(self):
         user = self.usermodel.objects.create_user(
@@ -16,6 +22,8 @@ class UsersManagersTests(TestCase):
         self.assertTrue(user.is_active)
         self.assertFalse(user.is_staff)
         self.assertFalse(user.is_superuser)
+        self.assertFalse(user.is_agent)
+        self.assertFalse(user.is_admin)
 
     def test_create_superuser(self):
         admin_user = self.usermodel.objects.create_superuser(
@@ -27,3 +35,12 @@ class UsersManagersTests(TestCase):
         self.assertTrue(admin_user.is_active)
         self.assertTrue(admin_user.is_staff)
         self.assertTrue(admin_user.is_superuser)
+        self.assertFalse(admin_user.is_agent)
+        self.assertFalse(admin_user.is_admin)
+
+    def test_api_endpoint_access(self):
+        response = self.client.get(reverse("user_list_create"))
+        self.assertEqual(response.status_code, 403)  # before login
+        self.client.login(username="demouser", password="password")
+        response = self.client.get(reverse("user_list_create"))
+        self.assertEqual(response.status_code, 200)  # after login
