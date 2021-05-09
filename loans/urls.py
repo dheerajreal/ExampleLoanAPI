ba567b2e4e29f811062e8fa40c8302258c977b83
Dheeraj <44266862+dheerajreal@users.noreply.github.com>
2021-05-09 18:37:03 +0530

    api views to create, update and approve loans

---
+++ loans/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import LoanCreateView, LoanUpdateView, loan_approve_view
+urlpatterns = [
+    path('', LoanCreateView.as_view(), name="loan_create"),
+    path('<int:pk>/edit/', LoanUpdateView.as_view(), name="loan_update"),
+    path('<int:pk>/approve/', loan_approve_view, name="loan_approve"),
+
+
+
+]
---
+++ loans/views.py
@@ -0,0 +1,32 @@
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from .serializers import LoanSerializer
+from .models import Loan
+from accounts.permissions import AgentPermission, AdminPermission, ExclusivelyAgentPermission
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics import get_object_or_404
+
+
+class LoanCreateView(generics.ListCreateAPIView):
+    queryset = Loan.objects.all()
+    serializer_class = LoanSerializer
+    permission_classes = [IsAuthenticated, AdminPermission | AgentPermission]
+
+    def perform_create(self, serializer):
+        serializer.save(agent=self.request.user)
+
+
+class LoanUpdateView(generics.RetrieveUpdateAPIView):
+    queryset = Loan.get_non_approved_loans()
+    serializer_class = LoanSerializer
+    permission_classes = [IsAuthenticated, ExclusivelyAgentPermission]
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated, AdminPermission])
+def loan_approve_view(request, pk):
+    loan_object = get_object_or_404(Loan.get_non_approved_loans(), pk=pk)
+    loan_object.admin = request.user
+    loan_object = loan_object.mark_approved()
+    return Response(LoanSerializer(loan_object).data)
