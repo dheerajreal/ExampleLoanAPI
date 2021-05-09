61118d1ac1453e8d3892c270c2f6cc2cb4e46c6e
Dheeraj <44266862+dheerajreal@users.noreply.github.com>
2021-05-09 16:35:01 +0530

    add some methods for loan class

--- loans/models.py
+++ loans/models.py
@@ -38,3 +38,34 @@ class Loan(models.Model):
     status = models.CharField(max_length=1, default=LoanState.New)
     emi = models.DecimalField(max_digits=5, decimal_places=0, default=0)
 
+    def mark_approved(self):
+        self.status = LoanState.Approved
+        self.save()
+
+    def mark_rejected(self):
+        self.status = LoanState.Rejected
+        self.save()
+
+    @property
+    def is_editable(self):
+        return self.status != LoanState.Approved
+
+    @classmethod
+    def get_all_loans(cls):
+        return cls.objects.all()
+
+    @classmethod
+    def get_rejected_loans(cls):
+        return cls.objects.filter(status=LoanState.Rejected)
+
+    @classmethod
+    def get_approved_loans(cls):
+        return cls.objects.filter(status=LoanState.Approved)
+
+    @classmethod
+    def get_new_requested_loans(cls):
+        return cls.objects.filter(status=LoanState.New)
+
+    @classmethod
+    def get_non_approved_loans(cls):
+        return cls.objects.exclude(status=LoanState.Approved)
