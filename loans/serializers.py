e2411bde96aa781a181620499c567d6179d0b4c0
Dheeraj <44266862+dheerajreal@users.noreply.github.com>
2021-05-09 18:08:40 +0530

    create serializer for loan

---
+++ loans/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from .models import Loan
+User = get_user_model()
+
+
+class LoanSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Loan
+        read_only_fields = ["status", "agent", "admin", "emi"]
+
+        fields = [
+            "id",
+            "beneficiary",
+            "requested_principal",
+            "interest_rate",
+            "duration_in_months",
+        ] + read_only_fields
